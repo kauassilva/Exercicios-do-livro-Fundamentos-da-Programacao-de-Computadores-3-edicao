7103f35b86186f55a46d91b76c98899e85a4a62b
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-08-09 15:07:40 -0300

    Add ExercicioProposto16 in capitulo_5

--- Livro_Fundamentos_da_Programacao_de_Computadores/src/capitulo_5/exercicios_propostos/ExercicioProposto16.java
+++ Livro_Fundamentos_da_Programacao_de_Computadores/src/capitulo_5/exercicios_propostos/ExercicioProposto16.java
@@ -1,14 +1,45 @@
 /**
  * @author kauas - 9/08/2022 terça
  * Livro: Fundamentos da Programação de Computadores
- * Capítulo 5, Exercício Proposto 16, pág. 14
+ * Capítulo 5, Exercício Proposto 16, pág. 148
  */
 
 /*
-
+Faça um programa que receba várias idades, calcule e mostre a média das idades
+digitadas. Finalize digitando idade igual a zero.
 */
 package capitulo_5.exercicios_propostos;
 
+import java.util.Scanner;
+
 public class ExercicioProposto16 {
     
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    int idade, i, media, soma;
+
+    i = 1;
+    soma = 0;
+
+    System.out.println("\nPara encerrar o sistema, digite 0 para idade.");
+    System.out.print("Digite a "+i+"ª idade: ");
+    idade = entrada.nextInt();
+
+    // Estrutura de repetição para a soma das idades
+    while (idade != 0) {
+      soma = soma + idade;
+
+      i++;
+      System.out.println("\nPara encerrar o sistema, digite 0 para idade.");
+      System.out.print("Digite a "+i+"ª idade: ");
+      idade = entrada.nextInt();
+    }
+
+    media = soma / (i-1);
+
+    System.out.println("\nMédia das idades: "+media+" anos\n");
+
+    entrada.close();
+  }
 }
