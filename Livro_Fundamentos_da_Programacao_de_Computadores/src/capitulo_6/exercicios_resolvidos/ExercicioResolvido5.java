d7b3a5552e241c13c5fc50af0e396eda020f32b7
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-09-03 23:13:55 -0300

    Complete ExercicioResolvido5

--- Livro_Fundamentos_da_Programacao_de_Computadores/src/capitulo_6/exercicios_resolvidos/ExercicioResolvido5.java
+++ Livro_Fundamentos_da_Programacao_de_Computadores/src/capitulo_6/exercicios_resolvidos/ExercicioResolvido5.java
@@ -14,7 +14,7 @@ os seguintes vetores resultantes:
   [imagem, pág. 162]  
 
 • A diferença entre X e Y
-  (todos os elementos que não existam em Y, sem repetições);
+  (todos os elementos de X que não existam em Y, sem repetições);
 
   [imagem, pág. 162]
 
@@ -42,38 +42,174 @@ public class ExercicioResolvido5 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        int tamanho, tamFinal;
+        int tamanho, tamFinal, contUniao, contDiferenca, contInterseccao, j, k;
         
         tamanho = 10;
         tamFinal = tamanho*2;
+        contUniao = 0;
+        contDiferenca = 0;
+        contInterseccao = 0;
         
         int[] vetorX = new int[tamanho];
         int[] vetorY = new int[tamanho];
         int[] vetorUniao = new int[tamFinal];
+        int[] vetorDiferenca = new int[tamanho];
+        int[] vetorSoma = new int[tamanho];
+        int[] vetorProduto = new int[tamanho];
+        int[] vetorInterseccao = new int[tamanho];
         
+        
+        
+        // Preenchimento dos vetor X
         System.out.println("--- Vetor X ---\n");
         for (int i=0; i<tamanho; i++) {
             System.out.print("Digite um número inteiro para o índice "+i+": ");
             vetorX[i] = entrada.nextInt();
         }
         
+        // Preenchimento do vetor Y
         System.out.println("\n--- Vetor Y ---\n");
         for (int i=0; i<tamanho; i++) {
             System.out.print("Digite um número inteiro para o índice "+i+": ");
             vetorY[i] = entrada.nextInt();
+        }   
+        
+        
+        
+        // União dos vetores X e Y sem repetições
+        for (int i=0; i<tamanho; i++) {
+            j = 0;
+            
+            while (j < contUniao && vetorX[i] != vetorUniao[j]) {
+                j++;
+            }
+            
+            if (j >= contUniao) {
+                vetorUniao[contUniao] = vetorX[i];
+                contUniao++;
+            }
         }
         
-        System.out.println("--- União dos vetores --- ");
-        /*for (int i=0; i<tamFinal; i++) {
-            int j = 0;
+        for (int i=0; i<tamanho; i++) {
+            j = 0;
             
-            while (j < ) {
-                
+            while (j < contUniao && vetorY[i] != vetorUniao[j]) {
+                j++;
             }
-        }*/
+            
+            if (j >= contUniao) {
+                vetorUniao[contUniao] = vetorY[i];
+                contUniao++;
+            }
+        }
+        
+        System.out.println("\n--- União dos vetores ---\n");
+        System.out.print("[");
+        for (int i=0; i<contUniao; i++) {
+            if (i != contUniao-1) {
+                System.out.print(vetorUniao[i]+", ");
+            } else {
+                System.out.print(vetorUniao[i]+"]");
+            }
+        }
         
         
-        System.out.println();
+        
+        // Diferença entre os vetores X e Y
+        for (int i=0; i<tamanho; i++) {
+            j = 0;
+            
+            while (j < tamanho && vetorX[i] != vetorY[j]) {
+                j++;
+            }
+            
+            if (j >= tamanho) {
+                k = 0;
+                
+                while (k < contDiferenca && vetorX[i] != vetorDiferenca[k]) {
+                    k++;
+                }
+                
+                if (k >= contDiferenca) {
+                    vetorDiferenca[contDiferenca] = vetorX[i];
+                    contDiferenca++;
+                }
+            }
+        }
+        
+        System.out.println("\n\n--- Diferença dos Vetores ---\n");
+        System.out.print("[");
+        for (int i=0; i<contDiferenca; i++) {
+            if (i != contDiferenca-1) {
+                System.out.print(vetorDiferenca[i]+", ");
+            } else {
+                System.out.print(vetorDiferenca[i]+"]");
+            }
+        }
+        
+        
+        
+        // Vetor de soma e produto entre os vetores X e Y
+        for (int i=0; i<tamanho; i++) {
+            vetorSoma[i] = vetorX[i] + vetorY[i];
+            vetorProduto[i] = vetorX[i] * vetorY[i];
+        }
+        
+        System.out.println("\n\n--- Soma dos vetores ---\n");
+        System.out.print("[");
+        for (int i=0; i<tamanho; i++) {
+            if (i != tamanho-1) {
+                System.out.print(vetorSoma[i]+", ");
+            } else {
+                System.out.print(vetorSoma[i]+"]");
+            }
+        }
+        
+        System.out.println("\n\n--- Produto dos vetores ---\n");
+        System.out.print("[");
+        for (int i=0; i<tamanho; i++) {
+            if (i != tamanho-1) {
+                System.out.print(vetorProduto[i]+", ");
+            } else {
+                System.out.print(vetorProduto[i]+"]");
+            }
+        }
+        
+        
+        
+        // Intersecção dos vetores X e Y
+        for (int i=0; i<tamanho; i++) {
+            j = 0;
+            
+            while (j < tamanho && vetorX[i] != vetorY[j]) {
+                j++;
+            }
+            
+            if (j < tamanho) {
+                k = 0;
+                
+                while (k < contInterseccao && vetorX[i] != vetorInterseccao[k]) {
+                    k++;
+                }
+                
+                if (k >= contInterseccao) {
+                    vetorInterseccao[contInterseccao] = vetorX[i];
+                    contInterseccao++;
+                }
+            }
+        }
+        
+        System.out.println("\n\n--- Intersecção dos vetores ---\n");
+        System.out.print("[");
+        for (int i=0; i<contInterseccao; i++) {
+            if (i != contInterseccao-1) {
+                System.out.print(vetorInterseccao[i]+", ");
+            } else {
+                System.out.print(vetorInterseccao[i]+"]");
+            }
+        }
+        
+        System.out.println("\n");
         
         entrada.close();
     }
